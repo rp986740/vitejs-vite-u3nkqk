33ec5d486ba9ae3393eb9938dcf72c5ff8d3606d
rp986740 <rp.stock@outlook.com>
2024-02-29 09:30:13 +0530

    added max limit and min search char

--- src/AutoComplete.tsx
+++ src/AutoComplete.tsx
@@ -10,17 +10,16 @@ const AutoComplete: React.FC<AutoCompleteProps> = ({ suggestions }) => {
   const [showSuggestions, setShowSuggestions] = useState<boolean>(false);
 
   useEffect(() => {
-    // Filter suggestions based on user input
     const filtered = suggestions.filter(
-      (suggestion) =>
+      suggestion =>
         suggestion.toLowerCase().indexOf(inputValue.toLowerCase()) > -1
     );
-    setFilteredSuggestions(filtered);
+    setFilteredSuggestions(filtered.slice(0, 10));
   }, [inputValue, suggestions]);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
-    setShowSuggestions(true);
+    setShowSuggestions(event.target.value.trim().split(/\s+/).length >= 1);
   };
 
   const handleSuggestionClick = (suggestion: string) => {
@@ -36,43 +35,51 @@ const AutoComplete: React.FC<AutoCompleteProps> = ({ suggestions }) => {
   return (
     <div>
       {showSuggestions && (
-        <div className="flex items-center flex-wrap max-w-lg gap-y-1 gap-x-1">
+        <div className="flex items-center flex-wrap max-w-lg gap-y-1 gap-x-1 transition-all duration-100 delay-100 ease-in-out">
           {filteredSuggestions.map((suggestion) => (
             <div
               className="cursor-pointer bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300"
               key={suggestion}
               onClick={() => handleSuggestionClick(suggestion)}
             >
-              {suggestion}
+              <p className='text-red-600 font-semibold text-sm hover:scale-125'>{suggestion}</p>
             </div>
           ))}
         </div>
       )}
-      <div className="flex relative items-center justify-center">
-        <input
-          className="mt-2 border-[2px] px-2 py-2 rounded-lg active:outline-none focus:outline-none w-full"
-          type="text"
-          value={inputValue}
-          onChange={handleInputChange}
-          onBlur={handleBlur}
-          placeholder="Start typing..."
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          className="lucide lucide-search absolute right-2 cursor-pointer mt-1"
-        >
-          <circle cx="11" cy="11" r="8" />
-          <path d="m21 21-4.3-4.3" />
-        </svg>
-      </div>
+
+    <div className="relative mt-2">
+      <label htmlFor="Search" className="sr-only"> Search </label>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        onBlur={handleBlur}
+        placeholder="Search for..."
+        className="border px-2 w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm"
+      />
+
+      <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+        <button type="button" className="text-gray-600 hover:text-gray-700">
+          <span className="sr-only">Search</span>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="h-4 w-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </span>
+    </div>
     </div>
   );
 };
